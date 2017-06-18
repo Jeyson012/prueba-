51f83db04b842b55865487a65a6c9c2d3f2ab2f4
Jeyson012 <jeysonpantoja@gmail.com>
2017-06-18 14:39:48 -0500

    del 1 al 10 con for (no sale)

---
+++ del 1 al 10 con for/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class del_1_al_10_con_for
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = Convert.ToInt32(Console.ReadLine());
+
+          
+            for(i=1; i<=10; i++)
+            {
+                Console.WriteLine(i);
+                Console.ReadKey();
+            }
+        }
+    }
+}
